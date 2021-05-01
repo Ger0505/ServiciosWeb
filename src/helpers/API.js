fc5ca2b465fdbd15529f236ab5d4450bb8d4fef8
Francisco Árciga Nolasco <Francisco@Kribrick.local>
2021-04-30 22:17:21 -0500

    Semi listo pedidos web

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,30 +1,38 @@
-const URL = "http://localhost:3001/"
-const URLFile = "http://localhost:3002/upload"
+const URL = "http://localhost:3001/";
+const URLFile = "http://localhost:3002/upload";
 
-class API{
-    async getBody(url, method, body){
-        let jsonObj = JSON.stringify(body)
-        const query = await fetch(URL + url,
-        {   method: method,
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-            body: jsonObj})
-        const data = query.json()
-        return data
-    }
+class API {
+  async getBody(url, method, body) {
+    let jsonObj = JSON.stringify(body);
+    const query = await fetch(URL + url, {
+      method: method,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: jsonObj,
+    });
+    const data = query.json();
+    return data;
+  }
 
-    async getData(url, method){
-        const query = await fetch(URL + url,
-        {   method: method,
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }})
-        const data = query.json()
-        return data
-    }
+  async getData(url, method) {
+    const query = await fetch(URL + url, {
+      method: method,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const data = query.json();
+    return data;
+  }
 
-    async getFile(file){
-        const formData = new FormData();
-        formData.append('file', file);
-        fetch(URLFile, { method: 'POST', body: formData })
-    }
+  async getFile(file) {
+    const formData = new FormData();
+    formData.append("file", file);
+    fetch(URLFile, { method: "POST", body: formData });
+  }
 }
 
 export default new API();
