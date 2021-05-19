5b84a414d27990d1a2d8256e2cef48c772cdf1b7
Francisco Árciga Nolasco <Francisco@Kribrick.local>
2021-05-18 21:13:37 -0500

    pull 18/05/2021

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,23 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import '../../pages/Login/Login.scss'
+import React, { useState, useEffect } from "react";
+import "../../pages/Login/Login.scss";
 import { CCard, CCardBody, CCardHeader } from "@coreui/react";
-import { API, FileURL } from "../../helpers"
-import { useForm, Controller } from "react-hook-form"
+import { API, FileURL } from "../../helpers";
+import { useForm, Controller } from "react-hook-form";
 
 const FileUpload = ({ actualLogo, cb }) => {
-  const [imageSrc, setImageSrc] = useState('')
-  const { register, handleSubmit, setValue,formState:{ errors } } = useForm()
+  const [imageSrc, setImageSrc] = useState("");
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
   useEffect(() => {
-    setImageSrc(actualLogo)
-    register("logo",{ required: { value: true, message: 'El logo es requerido' }})
-  }, [actualLogo, register])
+    setImageSrc(actualLogo);
+    register("logo", {
+      required: { value: true, message: "El logo es requerido" },
+    });
+  }, [actualLogo, register]);
 
-  const _onSubmit = async data => {
+  const _onSubmit = async (data) => {
     console.log(data);
-    await API.getFile(data.logo[0])
-    cb(data.logo[0].name)
-  }
+    await API.getFile(data.logo[0]);
+    cb(data.logo[0].name);
+  };
 
   return (
     <CCard>
@@ -25,26 +32,58 @@ const FileUpload = ({ actualLogo, cb }) => {
         <strong>Imagen de la empresa</strong>
       </CCardHeader>
       <CCardBody>
-        <form onSubmit={handleSubmit(_onSubmit)} encType="multipart/form-data" className="login100-form validate-form" style={{ paddingTop: 0 }}>
+        <form
+          onSubmit={handleSubmit(_onSubmit)}
+          encType="multipart/form-data"
+          className="login100-form validate-form"
+          style={{ paddingTop: 0 }}
+        >
           <div className="wrap-input100 validate-input">
-            <input className="input100 has-val" type="file" name="logo"
-              onChange={e => { setValue("logo", e.target.files) }}
+            <input
+              className="input100 has-val"
+              type="file"
+              name="logo"
+              onChange={(e) => {
+                setValue("logo", e.target.files);
+              }}
             />
             <span className="focus-input100"></span>
           </div>
-          {errors.logo && <small style={{ color: 'red' }}>{errors.logo?.message}</small>}
+          {errors.logo && (
+            <small style={{ color: "red" }}>{errors.logo?.message}</small>
+          )}
           <div className="container-login100-form-btn">
-            <button type="submit" className="login100-form-btn" style={{background: '#2a1ab9', color: '#fff', border: 'none', marginTop: '0.5em'}}>
+            <button
+              type="submit"
+              className="login100-form-btn"
+              style={{
+                background: "#2a1ab9",
+                color: "#fff",
+                border: "none",
+                marginTop: "0.5em",
+              }}
+            >
               Iniciar Sesión
             </button>
-        </div>
+          </div>
         </form>
-        <div style={{ marginBottom: 10, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <img style={{ width: '50%' }} src={FileURL + imageSrc} alt="logoImage" />
+        <div
+          style={{
+            marginBottom: 10,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            style={{ width: "50%" }}
+            src={FileURL + imageSrc}
+            alt="logoImage"
+          />
         </div>
       </CCardBody>
     </CCard>
-  )
-}
+  );
+};
 
-export default FileUpload
+export default FileUpload;
